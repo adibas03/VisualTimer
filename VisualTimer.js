9a55d53126d10bfbbfede8685ab67fc85a6aef80
adibas03 <adibas03@ymail.com>
2016-07-07 04:46:03 +0100

    Added Pulse and Notification option
    pulse: true,notify:true  
    To use notify, sound to be used for notification must be loaded as 'clock_sound'

--- VisualTimer.js
+++ VisualTimer.js
@@ -22,11 +22,17 @@
 		if(opts.group)this.group = opts.group;
 		this.onComplete = opts.onComplete;
 		var key = 'timer';
+		if(opts.notify)this.warn = this.game.add.audio('clock_sound');
+		if(opts.pulse)this.pulse = opts.pulse;
 		if (opts.key) {
 			key = opts.key;
 		}
 		(this.group)?this.group.create(opts.x, opts.y, key, 1):this.game.add.sprite(opts.x, opts.y, key, 1);
 		this.sprite = (this.group)?this.group.create(opts.x, opts.y, key, 0):this.game.add.sprite(opts.x, opts.y, key, 0);
+		if(this.pulse){
+			this.sprite.alpha = 0.2
+		this.game.add.tween(this.sprite).to( { alpha: 1 }, 1500, Phaser.Easing.Linear.None, true, 0, 800, true);
+			}
 		this.fullWidth = this.sprite.width;
 		this.reset();
 	}
@@ -42,6 +48,7 @@
 			this.timer.repeat(this.loop, this.totalTime, timerTick, this);
 			this.timer.onComplete.add(function() {
 				self.hasFinished = true;
+				if(self.warn)self.warn.stop();
 				if (self.onComplete) {
 					self.onComplete();
 				}
@@ -66,10 +73,12 @@
 
 		stop: function() {
 			this.timer.stop();
+			if(this.warn)this.warn.stop();
 		},
 
 		pause: function() {
 			this.timer.pause();
+			if(this.warn)if(this.warn.isPlaying)this.warn.stop;
 		},
 
 		resume: function() {
@@ -87,6 +96,7 @@
 		var myTime = (this.type == 'down') ? this.remainingTime() : this.timer.seconds;
 		this.rect.width = Math.max(0, (myTime / this.totalTime) * this.fullWidth);
 		this.sprite.crop(this.rect);
+		if(this.warn)if((myTime / this.totalTime) < (this.type == 'down' ?0.36:1-0.36))this.warn.isPlaying?'':this.warn.loopFull();
 	}
 
 
